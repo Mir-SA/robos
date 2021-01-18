0b6fa40ab6cfc87effa1602cc3ce90f01e9c80b0
Samir Supe <65391345+Mir-SA@users.noreply.github.com>
2021-01-18 08:08:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./containers/App";
-import tachyons from "tachyons";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import "tachyons";
+import "./index.css";
+import { searchRobots, requestRobots } from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
